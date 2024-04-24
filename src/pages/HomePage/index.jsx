483ce4a7694c3cdfb8577d2afe47c571ce604d82
aarreumii <140951595+aareumii@users.noreply.github.com>
2024-04-24 22:46:15 +0900

    feat : Category 컴포넌트 생성

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import FiltersCategory from './filter-category/FiltersCategory';
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div className="page">
+			<div className="container">
+				<h1>Products</h1>
+				<FiltersCategory />
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
