fbbd688c566ba0fbf76ee73e245e783c3b5bd63e
aarreumii <140951595+aareumii@users.noreply.github.com>
2024-04-27 23:25:27 +0900

    feat : 카테고리별 상품 데이터 가져오기 기능 구현

--- src/pages/HomePage/card-list/CardList.jsx
+++ src/pages/HomePage/card-list/CardList.jsx
@@ -3,14 +3,20 @@ import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
 import styles from './CardList.module.scss';
 import { fetchProducts } from '../../../store/products/products.slice';
 import CardItem from './card-item/CardItem';
+import CardSkeleton from '../card-skeleton/CardSkeleton';
 
 const CardList = () => {
 	const dispatch = useAppDispatch();
-	const { products } = useAppSelector((state) => state.productsSlice);
+	const { products, isLoading } = useAppSelector(
+		(state) => state.productsSlice
+	);
+	const category = useAppSelector((state) => state.categoriesSlice);
 
 	useEffect(() => {
-		dispatch(fetchProducts(styles));
-	}, []);
+		dispatch(fetchProducts(category?.toLowerCase()));
+	}, [category]);
+
+	if (isLoading) return <CardSkeleton />;
 
 	return (
 		<ul className={styles.card_list}>
--- src/store/products/products.slice.js
+++ src/store/products/products.slice.js
@@ -3,9 +3,16 @@ import axios from 'axios';
 
 export const fetchProducts = createAsyncThunk(
 	'products/fetchProducts', // Action type의 이름 수정
-	async (thunkAPI) => {
+	async (category, thunkAPI) => {
 		try {
-			const response = await axios.get(`https://fakestoreapi.com/products`);
+			let response;
+			if (category) {
+				response = await axios.get(
+					`https://fakestoreapi.com/products/category/${category}`
+				);
+			} else {
+				response = await axios.get(`https://fakestoreapi.com/products`);
+			}
 
 			return response.data;
 		} catch (error) {
