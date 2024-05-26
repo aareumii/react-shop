7dfdb58a25ffd201600a946255163a6f8ebc75dd
aarreumii <140951595+aareumii@users.noreply.github.com>
2024-05-26 22:41:10 +0900

    chore : rollup.config.ts 수정

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,9 +4,14 @@ import cssnano from 'cssnano';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import sass from 'rollup-plugin-sass';
 import path from 'path';
+import { defineConfig } from 'rollup';
+import url from 'url';
 
-export default {
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+export default defineConfig({
 	input: 'src/index.tsx',
 	output: {
 		file: 'dist/bundle.js',
@@ -17,22 +22,20 @@ export default {
 		nodeResolve(),
 		commonjs(),
 		typescript(),
+		sass({
+			include: ['**/*.scss'],
+			options: {
+				includePaths: [
+					path.resolve(__dirname, 'src/components'),
+					path.resolve(__dirname, 'node_modules'),
+				],
+			},
+		}),
 		postcss({
 			extensions: ['.css', '.scss'],
 			modules: true,
-			use: [
-				[
-					'sass',
-					{
-						includePaths: [
-							path.resolve(__dirname, 'src/components'),
-							path.resolve(__dirname, 'node_modules'),
-						],
-					},
-				],
-			],
 			plugins: [autoprefixer(), cssnano()],
 		}),
 	],
 	external: ['react', 'react-dom'],
-};
+});
